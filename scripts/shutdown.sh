fcea6aed51c421b6d3cac7c46f7fa8dc299d40dc
JonasRettig <114226512+JonasRettig@users.noreply.github.com>
2023-10-10 14:03:43 +0200

    Update shutdown.sh to add date into logs

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
+echo ------------------------------------------------------------------
+date
 kill $(ps aux | grep serve | awk '{print $2}')
 kill $(ps aux | grep chromium | awk '{print $2}')

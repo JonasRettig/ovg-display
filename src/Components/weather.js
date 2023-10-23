51a33b4f8bd89e7edb748fa3126bdb4d1ec61a13
Jonas Rettig <jrettig@uni-muenster.de>
2023-10-23 09:30:44 +0200

    some minor styling changes

--- src/Components/weather.js
+++ src/Components/weather.js
@@ -96,7 +96,7 @@ export default function Weather({weather, warnings, setWeatherWarningsExist, the
             case 4:
                 return "#880e4f"
             default:
-                return "#red"
+                return "#880e4f"
         }
     }
 
@@ -188,7 +188,7 @@ export default function Weather({weather, warnings, setWeatherWarningsExist, the
         <Stack key="warningsStack" direction={"row"} spacing={1} justifyContent={"center"}>
             {relevantWarnings.map((item) => {
                 return (
-                    <Card key={item.warnId}>
+                    <Card key={item.warnId}  elevation="0">
                         <CardContent key={item.warnId} style={{ backgroundColor: matchWarningColor(item.level) }}>
                             <Stack justifyContent={"center"} alignContent={"center"} alignItems={"center"} spacing={1}>
                                 <Typography variant="h4"> {item.event} von {buildTimestamp(item.start, true)} bis {buildTimestamp(item.end, true)} </Typography>
